7eb3d7cb51e6c17c0d3d7abc843e764bdd90b1f2
Linson-Lin <403786672@qq.com>
2015-06-05 00:29:08 +0800

    some update

--- TSC/WebContent/resources/js/manege-back/ps-tab.js
+++ TSC/WebContent/resources/js/manege-back/ps-tab.js
@@ -1,26 +1,52 @@
 /**
  * Created by LING on 2014/11/24.
  */
-$(function(){
-    var linkurl;
-    var $links = $('.main-side-nav-child');
-    for(var i=0; i<$links.size(); i++){
-        linkurl = $links.eq(i).attr("href");
-        if(window.location.href.indexOf(linkurl) != -1){
-            $links.eq(i).addClass("choose");
+window.FIX = window.FIX || {};
+
+FIX.fixbar = (function(){
+    var _e = {}, _p = $(window);
+    _e.set = function(para){
+        var obj = {};
+        obj.status = {
+            offsettop: $(para).offset().top
+        }
+        var scrollling = function(){
+            var st = _p.scrollTop();
+            if (st > obj.status.offsettop) {
+                $(para).addClass("fixed");
+            }else {
+                $(para).removeClass("fixed");
+            }
+        }
+
+        _p.on("scroll", scrollling);
+
+        return {
+            kill: function(){
+                _p.off("scroll", scrolling);
+            }
         }
     }
 
-   $('.main-side-nav-child').parent().hover(function(){
-            $(this).find(".ul-sub-side").stop(true, true).slideDown();
-        },function(){
-             $(this).find(".ul-sub-side").stop(true, true).slideUp();
+    return _e;
+})();
+
+$(function(){
+   FIX.fixbar.set('.main-side-nav');
+   $('.main-side-nav-child, .sub-side-nav').each(function(index, el) {
+    if(window.location.href.indexOf($(this).attr("href")) != -1){
+       $(this).addClass("choose");
+       if($(this).hasClass('sub-side-nav')){
+          $(this).parent().parent().parent().addClass('on');
         }
-    );
+    }
+   });
+
+   $('.dropdown_li>a').click(function(){
+        $(this).parent().toggleClass('on');
+    });
 
    	$(".dropdown_li .ul-sub-side .sub-side-nav").click(function(event) {
-   		/* Act on the event */
    		$(".right-content .tab").eq($(this).parent().index()).show().siblings().hide();
-//   		return false;
    	});
-})
+});
